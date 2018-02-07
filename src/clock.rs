6432512ed64ea4dbe1ace0a55b3dcf800da8268a
Axel Sundbom <axel-sundbom@hotmail.com>
2018-02-07 15:49:39 +0100

    Adds clock setup module.

---
+++ src/clock.rs
@@ -0,0 +1,45 @@
+#![allow(dead_code)]
+
+extern crate lpc1347;
+
+/// Configure the phase-locked-loop
+/// Some values of m and p may make the MCU crash, see page 15 and 44 of UM10524
+/// for details.
+///
+/// # Arguments
+/// * `mval` - Feedback divider value
+/// * `pval` - Post-divider ratio
+///
+/// # Example
+/// ```
+/// extern crate lpc1347;
+/// mod clock;
+/// use clock::pll_init;
+///
+/// fn init(p: init::Peripherals, r: init::Resources) {
+///     // See admissible values in p. 44 of UM10524
+///     pll_init(&p, 0b11, 0b01);
+/// }
+///
+/// ```
+pub unsafe fn pll_init (syscon: &lpc1347::SYSCON, mval: u8, pval: u8) {
+    if pval > 0x3 {
+        panic!("the PLL does not admit p-values higher than 0x3");
+    }
+
+    // Make sure the system oscillator is powered on, otherwise no system signal
+    syscon.pdawakecfg.modify(|_,w| w.sysosc_pd().bit(false));
+    syscon.pdruncfg.modify(|_,w| w.sysosc_pd().bit(false));
+
+    // Configure the PLL
+    syscon.pdruncfg.modify(|_,w| w.syspll_pd().bit(true));
+    syscon.syspllclksel.modify(|_, w| w.sel().bits(0x1));
+    syscon.syspllctrl.modify(|_, w| w.msel().bits(mval));
+    syscon.syspllctrl.modify(|_, w| w.psel().bits(pval));
+    syscon.pdruncfg.modify(|_,w| w.syspll_pd().bit(false));
+
+    // Before setting the clocks, wait for the PLL to lock
+    while !syscon.syspllstat.read().lock().bit() {}
+    syscon.mainclksel.modify(|_, w| w.sel().bits(0x3));
+    syscon.clkoutsel.modify(|_, w| w.sel().bits(0x3));
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
 extern crate lpc1347;
+pub mod clock;
 pub mod gpio;
 pub mod timers;
 pub mod uart;
